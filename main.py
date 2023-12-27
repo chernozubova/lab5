786c932765726de0e13ee2b0311d48b4aa38ff34
chernozubova <chenolika@mail.ru>
2023-12-27 15:53:13 +0700

    first stage

---
+++ main.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+
+def main():
+    while True:
+        print("\nМеню:")
+        print("1. Ввод исходных данных")
+        print("2. Решение задания")
+        print("3. Результат работы алгоритма")
+        print("4. Завершение работы программы")
+
+        point = input("Введите пункт меню: ")
+
+        if point == "1":
+            pass
+        elif point == "2":
+            pass
+        elif point == "3":
+            pass
+        elif point == "4":
+            pass
+        else:
+            pass
+
+
+if __name__ == 'main':
+    main()
